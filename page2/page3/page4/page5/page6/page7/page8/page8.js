b96d54c420ca5fb9bbb26ee79f1c5cdc27723fe9
coderBro <74422047+joshua761@users.noreply.github.com>
2023-10-08 18:58:09 +0530

    Add files via upload

---
+++ page2/page3/page4/page5/page6/page7/page8/page8.js
@@ -0,0 +1,8 @@
+document.getElementById("submitBtn").onclick(onclick) = function() {
+    // let userRate = document.getElementById("rating").value 
+    // if (userRate < 5) {
+    //     console.log("no !!!");
+    // }
+
+    console.log("hello");
+}
